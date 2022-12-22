311dc14a092878d9427c801bde8754fd7e02d1f2
IrynaVal <piv8050@gmail.com>
2022-12-22 23:21:36 +0200

    test

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,8 +2,8 @@ import Player from '@vimeo/player';
 
 console.log(Player);
 
-// test = 10;
-// console.log(test);
+const test = 10;
+console.log(test);
 
 
 // const player = new Player('handstick', {
