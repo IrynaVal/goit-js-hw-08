9217f8adff292fa6acc09f1c2adb288408c9df19
IrynaVal <piv8050@gmail.com>
2022-12-22 22:48:58 +0200

    add libs

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
 
+console.log(Player);
+
+// test = 10;
+// console.log(test);
+
+
+// const player = new Player('handstick', {
+//     id: 19231868,
+//     width: 640
+// });
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+
+// timeupdate
+// Triggered as the currentTime of the video updates. It generally fires every 250ms, but it may vary depending on the browser.
+
+// {
+//     duration: 61.857
+//     percent: 0.049
+//     seconds: 3.034
+// }
