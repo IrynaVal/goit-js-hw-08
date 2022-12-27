7e36f35f5fd9e34a0cb4d25c40eeb6f7ba25f636
IrynaVal <piv8050@gmail.com>
2022-12-27 02:00:22 +0200

    updated tasks 2-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,14 +4,30 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const CURRENT_TIME_KEY = "videoplayer-current-time";
-const currentTime = JSON.parse(localStorage.getItem(CURRENT_TIME_KEY));
+let currentTime = JSON.parse(localStorage.getItem(CURRENT_TIME_KEY));
 
 player.on('timeupdate', throttle(function (currentTime) {
     const seconds = currentTime.seconds;
     localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
 }, 1000));
 
-player.setCurrentTime(currentTime).then(function(seconds) {
+player.getDuration().then(function (duration) {
+    // duration = the duration of the video in seconds
+
+    if (currentTime + 1 > duration) {
+        currentTime = 0;
+    }
+
+    updateVideoTime();
+
+    // console.log('videoDuration', duration);
+    // console.log('currentTime', currentTime);
+}).catch(function (error) {
+    // an error occurred
+});
+
+const updateVideoTime = () => {
+    player.setCurrentTime(currentTime).then(function(seconds) {
     // seconds = the actual time that the player seeked to
 }).catch(function(error) {
     switch (error.name) {
@@ -24,3 +40,6 @@ player.setCurrentTime(currentTime).then(function(seconds) {
             break;
     }
 });
+}
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,12 +10,15 @@ form.addEventListener('submit', onFormSubmit);
 fillInForm();
 
 function saveData(evt) {
-    formData[evt.target.name] = evt.target.value;
+  formData[evt.target.name] = evt.target.value;
  
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
   }
 
 function onFormSubmit(evt) {
+  //  if (form.elements.email.value === "" || form.elements.message.value === "") {
+  //   return alert("Please fill in all the fields!");
+  // }
     evt.preventDefault();
     evt.currentTarget.reset();
     console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
@@ -23,15 +26,27 @@ function onFormSubmit(evt) {
 }
 
 function fillInForm() {
-      try {
-        const savedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-    
-        if (savedData) {
-            form.elements.email.value = savedData.email;
-            form.elements.message.value = savedData.message;
-         }
-    } catch (error) {
-        console.log(error.message);
+  try {
+    const savedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    console.log(savedData);
+
+    if (savedData) {
+      form.elements.email.value = savedData.email ?? '';
+      form.elements.message.value = savedData.message ?? ''; 
     }
-    }
-    
+  } catch (error) {
+          console.log(error.message);
+      }
+  }
+
+// function fillInForm() {
+//     const savedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+//     console.log(savedData);
+
+//     if (savedData) {       
+//       Object.entries(savedData).forEach(([key, value]) => {
+//         console.log(form[key].value = value);
+//         console.log(formData[key] = value);
+//     })
+//   };
+// }
